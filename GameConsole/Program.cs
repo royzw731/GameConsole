c85e29a0e5dbe208a9670dac8aab03bb9b908416
USER <USER@DESKTOP-2OKNQSC>
2025-12-16 13:44:35 +0200

    DFHDFHDFHD

--- GameConsole/Program.cs
+++ GameConsole/Program.cs
@@ -14,18 +14,7 @@ namespace GameConsole
             MenuGames b = new MenuGames();
             b.Show();
            
-            //List<IGamePlay> games = new List<IGamePlay>();
-            //games.Add(new Games.TetrisGame());
-            //games.Add(new Games.FluffyBirdGame());
-            //games.Add(new Games.PacManGame());
-
-            //foreach (var game in games)
-            //{
-            //    game.Play();
-            //	Console.Write($" Game:{game.Name}");
-            //	Console.WriteLine($"Score:{game.Score}");
-            //}
-
+           
         }
     }
 }
