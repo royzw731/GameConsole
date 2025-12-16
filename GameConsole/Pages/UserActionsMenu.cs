1fd8b1be29cedf1069b5a94c5a02f801dd7c0e7b
USER <USER@DESKTOP-2OKNQSC>
2025-12-16 13:33:30 +0200

    sgsdgsdgsdFGSDG

--- GameConsole/Pages/UserActionsMenu.cs
+++ GameConsole/Pages/UserActionsMenu.cs
@@ -7,7 +7,7 @@ namespace GameConsole.Pages
     {
         private User user;
 
-        public UserActionsMenu(string title, User user) : base(title)
+        public UserActionsMenu(string title, User user) : base("WRLCOME TO USER ACTIONS MENU!")
         {
             this.user = user;
             Add("Update User Name & Password", new UpdateUserName("Update User Details"));
